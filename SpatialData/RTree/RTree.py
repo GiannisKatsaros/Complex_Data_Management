ee3085843c1fa31f22bf58a191d37ab9fe2b696a
GiannisKatsaros <katsaros97giannis@gmail.com>
2023-08-20 19:11:29 +0300

    ✨Calculate mbr

--- SpatialData/RTree/RTree.py
+++ SpatialData/RTree/RTree.py
@@ -1,5 +1,59 @@
-with open("../data/inputs/coords.txt") as fc:
-    with open("../data/inputs/offsets.txt") as fo:
-        with open("../data/outputs/rtree.txt") as fr:
-            coords = []
-            offsets = []
+class Polygon:
+    def __init__(self, coords, id):
+        self.coords = coords
+        self.id = id
+
+    def calculateMBR(self):
+        self.minX = self.coords[0][0]
+        self.minY = self.coords[0][1]
+        self.maxX = self.coords[0][0]
+        self.maxY = self.coords[0][1]
+        for coordinate in self.coords:
+            if coordinate[0] < self.minX:
+                self.minX = coordinate[0]
+            elif coordinate[0] > self.maxX:
+                self.maxX = coordinate[0]
+            if coordinate[1] < self.minY:
+                self.minY = coordinate[1]
+            elif coordinate[1] > self.maxY:
+                self.maxY = coordinate[1]
+        return [
+            [self.minX, self.minY],
+            [self.maxX, self.minY],
+            [self.maxX, self.maxY],
+            [self.minX, self.maxY],
+        ]
+
+
+def main():
+    with open("../data/inputs/coords.txt") as fc:
+        with open("../data/inputs/offsets.txt") as fo:
+            with open("../data/outputs/rtree.txt", "w") as fr:
+                coords = []
+                offsets = []
+                polygons = []
+                for line in fc:
+                    xCoord, yCoord = line.strip("\n").split(",")
+                    xCoord = float(xCoord)
+                    yCoord = float(yCoord)
+                    coords.append([xCoord, yCoord])
+
+                for line in fo:
+                    id, start, end = line.strip("\n").split(",")
+                    id = int(id)
+                    start = int(start)
+                    end = int(end)
+                    offsets.append([id, start, end])
+
+                for i in range(len(offsets)):
+                    polygons.append(
+                        Polygon(
+                            coords[offsets[i][1] : offsets[i][2]],
+                            offsets[i][0],
+                        )
+                    )
+                print(polygons[0].calculateMBR())
+
+
+if __name__ == "__main__":
+    main()
