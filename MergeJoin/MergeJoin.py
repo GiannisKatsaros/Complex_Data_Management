0e474f2b7d846004ea634e4d6d8a8c719acc0893
GiannisKatsaros <katsaros97giannis@gmail.com>
2023-08-20 14:50:10 +0300

    🔧Refactor result writing without list

--- MergeJoin/MergeJoin.py
+++ MergeJoin/MergeJoin.py
@@ -1,43 +1,36 @@
-ans = []
-currentId = ''
+currentId = ""
 buffer = []
 maxBufferLength = 0
 with open("R_sorted.tsv") as fr:
     with open("S_sorted.tsv") as fs:
-        lineR = fr.readline()
-        lineS = fs.readline()
-        while lineR != "":
-            lineR = lineR.strip('\n')
-            listR = lineR.split('\t')
-            lineRId = listR[0]
-            lineS = lineS.strip('\n')
-            listS = lineS.split('\t')
-            lineSId = listS[0]
-            if lineRId == currentId and lineRId != lineSId:
-                ans.append(
-                    [currentId] + listR[1:] + buffer)
-                lineR = fr.readline()
-            elif lineRId == lineSId:
-                if lineRId != currentId:
-                    buffer = []
-                    currentId = lineRId
-                buffer.extend(listS[1:])
-                lineS = fs.readline()
-            elif lineRId < lineSId:
-                if lineRId != currentId:
+        with open("RSMergeJoin.tsv", "w") as fj:
+            lineR = fr.readline()
+            lineS = fs.readline()
+            while lineR != "":
+                lineR = lineR.strip("\n")
+                listR = lineR.split("\t")
+                lineRId = listR[0]
+                lineS = lineS.strip("\n")
+                listS = lineS.split("\t")
+                lineSId = listS[0]
+                if lineRId == currentId and lineRId != lineSId:
+                    fj.write("\t".join([currentId] + listR[1:] + buffer) + "\n")
+                    lineR = fr.readline()
+                elif lineRId == lineSId:
+                    if lineRId != currentId:
+                        buffer = []
+                        currentId = lineRId
+                    buffer.extend(listS[1:])
+                    lineS = fs.readline()
+                elif lineRId < lineSId:
+                    if lineRId != currentId:
+                        if len(buffer) > maxBufferLength:
+                            maxBufferLength = len(buffer)
+                        buffer = []
+                    lineR = fr.readline()
+                elif lineRId > lineSId:
                     if len(buffer) > maxBufferLength:
                         maxBufferLength = len(buffer)
                     buffer = []
-                lineR = fr.readline()
-            elif lineRId > lineSId:
-                if len(buffer) > maxBufferLength:
-                    maxBufferLength = len(buffer)
-                buffer = []
-                lineS = fs.readline()
-
-with open("RSMergeJoin.tsv", "w") as fj:
-    fj.write("Max Buffer Size: " + str(maxBufferLength) + '\n\n')
-    for i in ans:
-        fj.write('\t'.join(i) + '\n')
-        print(i)
-    print("Max Buffer Size: " + str(maxBufferLength))
+                    lineS = fs.readline()
+            fj.write("\nMax Buffer Size: " + str(maxBufferLength) + "\n")
