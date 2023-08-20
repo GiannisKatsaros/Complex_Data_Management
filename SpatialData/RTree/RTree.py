612bbeed386f7392b02ce563e49398bbf2cc1de0
GiannisKatsaros <katsaros97giannis@gmail.com>
2023-08-20 18:35:47 +0300

    🔧Directory organization

---
+++ SpatialData/RTree/RTree.py
@@ -0,0 +1,5 @@
+with open("../data/inputs/coords.txt") as fc:
+    with open("../data/inputs/offsets.txt") as fo:
+        with open("../data/outputs/rtree.txt") as fr:
+            coords = []
+            offsets = []
