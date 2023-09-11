c2e816add958603a9d22678031b43251c7d308cd
GiannisKatsaros <katsaros97giannis@gmail.com>
2023-09-12 02:16:12 +0300

    ✨Road network init

---
+++ SpatialNetworks/RoadNetwork/RoadNetwork.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, id, x, y):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.edges = []
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
